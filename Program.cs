4788433c6f5d566aacddc105b49b7624d2cc24a2
user <user@LAPTOP-QQLM7U2R>
2024-05-29 09:24:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romertal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input;
+                Console.WriteLine("1-Rom til Int\n2-Int til Rom\n3-Quit");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Skriv et romartal");
+                        string roman1 = Console.ReadLine().ToUpper();
+                        Console.WriteLine(Roman.RomanToInteger(roman1));
+                        break;
+                    case "2":
+                        Console.WriteLine("Skriv et tal");
+                        string str1 = Console.ReadLine();
+                        int tal1 = Convert.ToInt32(str1);
+                        Console.WriteLine(Roman.ToRoman(tal1));
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
